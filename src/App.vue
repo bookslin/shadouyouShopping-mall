5ba5177f79729739d73883a2f8e2b1ce96d38426
unknown <573363068@qq.com>
2023-06-05 23:45:14 +0800

    axios的基础封装

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-<el-button type="primary">Primary</el-button>
-<el-button type="success">Success</el-button>
+    <el-button type="primary">Primary</el-button>
 </template>
 
 <script setup>
@@ -9,4 +8,4 @@
 
 <style lang="less" scoped>
 
-</style>
+</style> 
--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 //element plus按需导入
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import vue from '@vitejs/plugin-vue'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
-        //...
+        //element
         AutoImport({
             resolvers: [ElementPlusResolver()],
         }),
         Components({
-            resolvers: [ElementPlusResolver()],
+            resolvers: [
+                ElementPlusResolver({ importStyle: "sass" }),
+
+            ],
         }),
 
     ],
@@ -25,5 +29,15 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src',
                 import.meta.url))
         }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // 自动导入定制化样式文件进行样式覆盖
+                additionalData: `
+            @use "@/styles/element/index.scss" as *;
+          `,
+            }
+                                 }
     }
 })
