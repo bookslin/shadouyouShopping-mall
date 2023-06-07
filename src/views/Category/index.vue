41c12a75b67adab1f2c05689fe42893b61ea28f8
unknown <573363068@qq.com>
2023-06-07 21:24:09 +0800

    一级分类 -路由配置

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="home">
+
     <h2>我是分类页面</h2>
-  </div>
+  
 </template>
 
 <script setup>
--- src/views/Layout/components/LayoutFixed.vue
+++ src/views/Layout/components/LayoutFixed.vue
@@ -21,7 +21,7 @@ import { useScroll } from '@vueuse/core'
           <RouterLink to="/">首页</RouterLink>
         </li>
         <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
-          <RouterLink to="/">{{ item.name}}</RouterLink>
+          <RouterLink :to="`/category/${ item.id }`">{{ item.name}}</RouterLink>
         </li>
       </ul>
 
