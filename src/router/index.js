e10c148cf028f7e7792160f2986fc31a3dc60e32
unknown <573363068@qq.com>
2023-06-10 20:12:26 +0800

    详情基础数据渲染

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Layout from '@/views/Layout/index.vue'
 import Category from '@/views/Category/index.vue'
 import Home from '@/views/Home/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
+import Detail from "@/views/Detail/index.vue"
 
 
 const router = createRouter({
@@ -24,6 +25,10 @@ const router = createRouter({
                     path: 'category/sub/:id',
                     component: SubCategory
                 },
+                {
+                    path: 'detail/:id',
+                    component: Detail
+                }
 
             ]
         },
@@ -32,7 +37,12 @@ const router = createRouter({
             component: Login
         }
 
-    ]
+    ],
+
+    //路由滚动行为定制
+    scrollBehavior() {
+        return { top: 0 }
+    }
 })
 
 export default router
